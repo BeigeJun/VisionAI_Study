7d14229367d53161b88a326d382480d2a8c92923
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-08-16 23:27:48 +0900

    Create MobileNetV2.py

---
+++ Pytorch/Classification/MobileNetV2.py
@@ -0,0 +1,56 @@
+def dwise_conv(ch_in, stride=1):
+    return (
+        nn.Sequential(
+            #depthwise
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
+            nn.BatchNorm2d(ch_in),
+            nn.ReLU6(inplace=True),
+        )
+    )
+
+def conv1x1(ch_in, ch_out):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+def conv3x3(ch_in, ch_out, stride):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+class InvertedBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, expand_ratio, stride):
+        super(InvertedBlock, self).__init__()
+
+        self.stride = stride
+        assert stride in [1,2]
+
+        hidden_dim = ch_in * expand_ratio
+
+        self.use_res_connect = self.stride==1 and ch_in==ch_out
+
+        layers = []
+        if expand_ratio != 1:
+            layers.append(conv1x1(ch_in, hidden_dim))
+        layers.extend([
+            #dw
+            dwise_conv(hidden_dim, stride=stride),
+            #pw
+            conv1x1(hidden_dim, ch_out)
+        ])
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.layers(x)
+        else:
+            return self.layers(x)
