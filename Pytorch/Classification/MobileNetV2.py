7a75489e49c6a7c48666a0deff3d4feaaa9eaa7f
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-09-03 23:29:46 +0900

    Update MobileNetV2.py

--- Pytorch/Classification/MobileNetV2.py
+++ Pytorch/Classification/MobileNetV2.py
@@ -1,49 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import optuna
+
+
 def dwise_conv(ch_in, stride=1):
-    return (
-        nn.Sequential(
-            #depthwise
-            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
-            nn.BatchNorm2d(ch_in),
-            nn.ReLU6(inplace=True),
-        )
+    return nn.Sequential(
+        nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
+        nn.BatchNorm2d(ch_in),
+        nn.ReLU6(inplace=True),
     )
+
 
 def conv1x1(ch_in, ch_out):
-    return (
-        nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
-        )
+    return nn.Sequential(
+        nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
+        nn.BatchNorm2d(ch_out),
+        nn.ReLU6(inplace=True)
     )
 
+
 def conv3x3(ch_in, ch_out, stride):
-    return (
-        nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
-        )
+    return nn.Sequential(
+        nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
+        nn.BatchNorm2d(ch_out),
+        nn.ReLU6(inplace=True)
     )
+
 
 class InvertedBlock(nn.Module):
     def __init__(self, ch_in, ch_out, expand_ratio, stride):
         super(InvertedBlock, self).__init__()
 
         self.stride = stride
-        assert stride in [1,2]
+        assert stride in [1, 2]
 
         hidden_dim = ch_in * expand_ratio
-
-        self.use_res_connect = self.stride==1 and ch_in==ch_out
+        self.use_res_connect = self.stride == 1 and ch_in == ch_out
 
         layers = []
         if expand_ratio != 1:
             layers.append(conv1x1(ch_in, hidden_dim))
         layers.extend([
-            #dw
             dwise_conv(hidden_dim, stride=stride),
-            #pw
             conv1x1(hidden_dim, ch_out)
         ])
 
@@ -55,11 +57,12 @@ class InvertedBlock(nn.Module):
         else:
             return self.layers(x)
 
+
 class MobileNetV2(nn.Module):
-    def __init__(self, ch_in=3, n_classes=1000):
+    def __init__(self, ch_in=3, n_classes=10, alpha=1.0):
         super(MobileNetV2, self).__init__()
 
-        self.configs=[
+        self.configs = [
             # t, c, n, s
             [1, 16, 1, 1],
             [6, 24, 2, 2],
@@ -70,30 +73,111 @@ class MobileNetV2(nn.Module):
             [6, 320, 1, 1]
         ]
 
-        self.stem_conv = conv3x3(ch_in, 32, stride=2)
+        input_channel = int(32 * alpha)
+        last_channel = int(1280 * alpha)
+
+        self.stem_conv = conv3x3(ch_in, input_channel, stride=2)
 
         layers = []
-        input_channel = 32
         for t, c, n, s in self.configs:
+            output_channel = int(c * alpha)
             for i in range(n):
                 stride = s if i == 0 else 1
-                layers.append(InvertedBlock(ch_in=input_channel, ch_out=c, expand_ratio=t, stride=stride))
-                input_channel = c
+                layers.append(InvertedBlock(ch_in=input_channel, ch_out=output_channel, expand_ratio=t, stride=stride))
+                input_channel = output_channel
 
         self.layers = nn.Sequential(*layers)
-
-        self.last_conv = conv1x1(input_channel, 1280)
-
+        self.last_conv = conv1x1(input_channel, last_channel)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.classifier = nn.Sequential(
             nn.Dropout2d(0.2),
-            nn.Linear(1280, n_classes)
+            nn.Linear(last_channel, n_classes)
         )
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x):
         x = self.stem_conv(x)
         x = self.layers(x)
         x = self.last_conv(x)
-        x = self.avg_pool(x).view(-1, 1280)
+        x = self.avg_pool(x).view(-1, self.classifier[1].in_features)
         x = self.classifier(x)
         return x
+
+
+def objective(trial):
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    alpha = trial.suggest_float("alpha", 0.5, 1.5)
+    lr = trial.suggest_float("lr", 1e-4, 1e-2, log=True)
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "SGD"])
+
+    model = MobileNetV2(alpha=alpha).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    if optimizer_name == "Adam":
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    else:
+        optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    return accuracy
+
+
+def main():
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=50)
+
+    print("Best trial:")
+    trial = study.best_trial
+    print(f"  Accuracy: {trial.value}")
+    print("  Best hyperparameters: ", trial.params)
+
+
+if __name__ == '__main__':
+    main()
