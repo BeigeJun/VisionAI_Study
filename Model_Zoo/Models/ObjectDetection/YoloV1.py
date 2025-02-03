448f090a47507dbe5876d2599ced0383ecdecb42
Seo_Jun_ <wns201401@naver.com>
2025-02-04 00:01:18 +0900

    YoloV1 추가

---
+++ Model_Zoo/Models/ObjectDetection/YoloV1.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+
+
+class Yolov1(nn.Module):
+    def __init__(self, split_size, num_boxes, num_classes=20):
+        super().__init__()
+        self.cfg = [
+            # Input, Output, Kernel, Stride, Pool
+            [3, 64, 7, 2, True],
+            [64, 192, 3, 1, True],
+            [192, 128, 1, 1, False],
+            [128, 256, 3, 1, False],
+            [256, 256, 1, 1, False],
+            [256, 512, 3, 1, True],
+            [512, 256, 1, 1, False],
+            [256, 512, 3, 1, False],
+            [512, 256, 1, 1, False],
+            [256, 512, 3, 1, False],
+            [512, 256, 1, 1, False],
+            [256, 512, 3, 1, False],
+            [512, 256, 1, 1, False],
+            [256, 512, 3, 1, False],
+            [512, 512, 1, 1, False],
+            [512, 1024, 3, 1, True],
+            [1024, 512, 1, 1, False],
+            [512, 1024, 3, 1, False],
+            [1024, 512, 1, 1, False],
+            [512, 1024, 3, 1, False],
+            [1024, 1024, 3, 1, False],
+            [1024, 1024, 3, 2, False],
+            [1024, 1024, 3, 1, False],
+            [1024, 1024, 3, 1, False]
+        ]
+        layers = []
+        for Input, Output, Kernel, Stride, Pool in self.cfg:
+            layers.append(nn.Conv2d(in_channels=Input, out_channels=Output,
+                                    kernel_size=Kernel, stride=Stride, padding=Kernel//2))
+            layers.append(nn.BatchNorm2d(Output))
+            layers.append(nn.LeakyReLU(0.1))
+            if Pool:
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.First_layer = nn.Sequential(*layers)
+        self.Second_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * split_size * split_size, 496),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+            nn.Linear(496, split_size * split_size * (5 * num_boxes + num_classes))
+        )
+
+    def forward(self, x):
+        out = self.First_layer(x)
+        return self.Second_layer(out)
