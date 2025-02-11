d6b094e5c9eba96cec97b6d0d11d574135e49444
Seo_Jun_ <wns201401@naver.com>
2025-02-11 22:22:04 +0900

    Runner : Detection 추가 및 그래프 기능 Class화

--- Model_Zoo/Runner.py
+++ Model_Zoo/Runner.py
@@ -3,6 +3,9 @@ from tqdm import tqdm
 import torch.utils.data
 import torch.optim as optim
 import matplotlib.pyplot as plt
+from torch.utils.data import random_split
+from Model_Zoo.Models.Util.Draw_Graph import Draw_Graph
+from Model_Zoo.Models.ObjectDetection.Util.Utils import calculate_IoU, mAP, get_bboxes, YoloLoss
 from DataLoader import Classification_data_loader, Object_Detection_data_loader, Compose
 from torchvision.datasets import ImageFolder
 from Models.Classification.MobileNetV1 import MobileNetV1
@@ -13,157 +16,96 @@ from Models.Classification.EfficientNet import EfficientNet
 from Models.ObjectDetection.YoloV1 import Yolov1
 
 
-def train_model(device, model, epochs, patience, train_loader, validation_loader, test_loader, save_path):
+def train_model(device, model, model_type, epochs, validation_epoch, learning_rate, patience, train_loader, validation_loader,
+                test_loader, save_path):
+    graph = Draw_Graph(save_path, patience)
+
     patience_count = 0
-    train_losses = []
-    train_accuracies = []
-    val_losses = []
-    val_accuracies = []
-
-    top_accuracy_train = 0
-    top_accuracy_validation = 0
-    top_accuracy_train_epoch = 0
-    top_accuracy_validation_epoch = 0
-
-    bottom_loss_train = float('inf')
-    bottom_loss_validation = float('inf')
-    bottom_loss_train_epoch = 0
-    bottom_loss_validation_epoch = 0
-
-    os.makedirs(save_path, exist_ok=True)
-
-    plt.ion()
-    fig, axs = plt.subplots(1, 2, figsize=(16, 4))
 
     model.train()
+    #criterion, optimizer 선택 가능하게 변경 필요
+    if model_type == "Classification":
+        criterion = torch.nn.CrossEntropyLoss()
+    elif model_type == "Object_Detection":
+        criterion = YoloLoss()
 
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     pbar = tqdm(range(epochs), desc="Epoch Progress")
+
     for epoch in pbar:
-        running_loss = 0.0
-        correct_train = 0
-        total_train = 0
+        if model_type == "Classification":
+            running_loss = 0.0
+            correct_train = 0
+            total_train = 0
 
-        if patience_count >= patience:
-            plt.savefig(save_path + '//training_validation_graphs.png')
-            plt.close()
-            break
-        for inputs, labels in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            if patience_count >= patience:
+                graph.save_plt()
+                break
 
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total_train += labels.size(0)
-            correct_train += (predicted == labels).sum().item()
+            for inputs, labels in train_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-        epoch_loss = running_loss / len(train_loader)
-        epoch_accuracy = 100 * correct_train / total_train
-        pbar.set_postfix({'Loss': f'{epoch_loss:.4f}', 'Accuracy': f'{epoch_accuracy:.2f}%'})
+                running_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                total_train += labels.size(0)
+                correct_train += (predicted == labels).sum().item()
 
-        patience_count += 1
-        train_loss = running_loss / len(train_loader)
-        train_accuracy = correct_train / total_train
-
-        if top_accuracy_train < train_accuracy:
-            top_accuracy_train = train_accuracy
-            model_path_make = save_path + '//Best_Accuracy_Train_MLP.pth'
-            torch.save(model.state_dict(), model_path_make)
-            top_accuracy_train_epoch = epoch
-
-        if bottom_loss_train > running_loss:
-            bottom_loss_train = running_loss
-            model_path_make = save_path + '//Bottom_Loss_Train_MLP.pth'
-            torch.save(model.state_dict(), model_path_make)
-            bottom_loss_train_epoch = epoch
-
-        if (epoch + 1) % 10 == 0:
-            train_losses.append(running_loss / len(train_loader))
+            train_loss = running_loss / len(train_loader)
             train_accuracy = correct_train / total_train
-            train_accuracies.append(train_accuracy)
 
+            pbar.set_postfix({'Loss': f'{train_loss:.4f}', 'Accuracy': f'{train_accuracy * 100:.2f}%'})
+
+            patience_count += 1
+            train_accuracy = correct_train / total_train
+
+            graph.save_train_best_model_info(model, epoch, train_accuracy, train_loss)
+
+            if (epoch + 1) % validation_epoch == 0:
+                graph.append_train_losses_and_acc(train_loss, train_accuracy)
+                graph.update_graph(model, device, validation_loader, criterion, epoch, model_type)
+                patience_count = graph.save_validation_best_model_info(model, epoch, patience_count)
+                graph.save_train_info(patience_count)
+
+        elif model_type == "Object_Detection":
+            model.train()
+            running_loss = 0.0
+            train_mAP = 0.0
+
+            for batch_idx, (x, y) in enumerate(train_loader):
+                x, y = x.to(device), y.to(device)
+                optimizer.zero_grad()
+                out = model(x)
+                loss = criterion(out, y)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            train_loss = running_loss / len(train_loader)
+
+            # Calculate train mAP
             model.eval()
-            val_loss = 0.0
-            correct_val = 0
-            total_val = 0
+            pred_boxes, target_boxes = get_bboxes(train_loader, model, iou_threshold=0.5, threshold=0.4, device=device)
+            train_mAP = mAP(pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint")
 
-            with torch.no_grad():
-                for inputs, labels in validation_loader:
-                    inputs, labels = inputs.to(device), labels.to(device)
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
+            pbar.set_postfix({'Train Loss': f'{train_loss:.4f}', 'Train mAP': f'{train_mAP:.4f}'})
 
-                    val_loss += loss.item()
-                    _, predicted = torch.max(outputs.data, 1)
-                    total_val += labels.size(0)
-                    correct_val += (predicted == labels).sum().item()
+            patience_count += 1
+            graph.save_train_best_model_info(model, epoch, train_mAP, train_loss)
 
-            val_losses.append(val_loss / len(validation_loader))
-            val_accuracy = correct_val / total_val
-            val_accuracies.append(val_accuracy)
+            if (epoch + 1) % validation_epoch == 0:
+                graph.append_train_losses_and_acc(train_loss, train_mAP)
+                graph.update_graph(model, device, validation_loader, criterion, epoch, model_type)
+                patience_count = graph.save_validation_best_model_info(model, epoch, patience_count)
+                graph.save_train_info(patience_count)
 
-            axs[0].clear()
-            axs[1].clear()
-
-            axs[0].plot(range(10, epoch + 2, 10), train_accuracies, label='Train Accuracy', color='red', linewidth=0.5)
-            axs[0].plot(range(10, epoch + 2, 10), val_accuracies, label='Validation Accuracy', color='blue',
-                        linewidth=0.5)
-            axs[0].set_xlabel('Epochs')
-            axs[0].set_ylabel('Accuracy')
-            axs[0].set_title('Training and Validation Accuracy')
-            axs[0].legend()
-
-            axs[1].plot(range(10, epoch + 2, 10), train_losses, label='Train Loss', color='red', linewidth=0.5)
-            axs[1].plot(range(10, epoch + 2, 10), val_losses, label='Validation Loss', color='blue', linewidth=0.5)
-            axs[1].set_xlabel('Epochs')
-            axs[1].set_ylabel('Loss')
-            axs[1].set_title('Training and Validation Loss')
-            axs[1].legend()
-
-            for tick in axs[0].get_xticks():
-                axs[0].axvline(x=tick, color='gray', linestyle='-', linewidth=0.1)
-
-            for tick in axs[0].get_yticks():
-                axs[0].axhline(y=tick, color='gray', linestyle='-', linewidth=0.1)
-
-            for tick in axs[1].get_xticks():
-                axs[1].axvline(x=tick, color='gray', linestyle='-', linewidth=0.1)
-
-            for tick in axs[1].get_yticks():
-                axs[1].axhline(y=tick, color='gray', linestyle='-', linewidth=0.1)
-
-            plt.draw()
-            plt.pause(0.1)
-
-            if bottom_loss_validation > val_loss:
-                bottom_loss_validation = val_loss
-                model_path_make = save_path + '//Bottom_Loss_Validation_MLP.pth'
-                torch.save(model.state_dict(), model_path_make)
-                bottom_loss_validation_epoch = epoch
-                patience_count = 0
-
-            if top_accuracy_validation < val_accuracy:
-                top_accuracy_validation = val_accuracy
-                model_path_make = save_path + '//Best_Accuracy_Validation_MLP.pth'
-                torch.save(model.state_dict(), model_path_make)
-                top_accuracy_validation_epoch = epoch
-
-            if (epoch + 1) % 50 == 0:
-                plt.savefig(save_path + '//training_validation_graphs.png')
-
-                with open(save_path + '//numbers.txt', "w") as file:
-                    file.write(
-                        f"Top Accuracy Train Epoch : {top_accuracy_train_epoch} Accuracy : {top_accuracy_train}\n"
-                        f"Top Accuracy Validation Epoch : {top_accuracy_validation_epoch} Accuracy : {top_accuracy_validation}\n"
-                        f"Bottom Loss Train Epoch : {bottom_loss_train_epoch} Loss : {bottom_loss_train}\n"
-                        f"Bottom Loss Validation Epoch : {bottom_loss_validation_epoch} Loss : {bottom_loss_validation}\n"
-                        f"Patience Count : {patience_count}/{patience}\n")
+            model.train()
 
     model.eval()
     with torch.no_grad():
@@ -179,19 +121,22 @@ def train_model(device, model, epochs, patience, train_loader, validation_loader
 
         accuracy = 100 * correct / total
 
-    with open(save_path + '//Test_Result.txt', "w") as file:
-        file.write(f"Total Num : {total}, Correct Num : {correct}\n"
-                   f"Accuracy : {accuracy}")
+    graph.save_test_info(total, correct, accuracy)
 
 
 def main():
-    num_class = 3
+    num_class = 20
     epoch = 10000
+    validation_epoch = 10
     patience = 1000
+    batch_size = 32
+    learning_rate = 0.001
     save_path = "D:/Model_Save/Test"
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Yolov1(split_size=7, num_boxes=2).to(device)
+    model = EfficientNet(num_class=num_class).to(device)
+    # model = Yolov1(split_size=7, num_boxes=2, num_classes=num_class).to(device)
+
 
     model_name = model.__class__.__name__
     model_type = "Object_Detection" if model_name == 'Yolov1' else "Classification"
@@ -199,19 +144,29 @@ def main():
     transform_info = model.return_transform_info()
     if model_type == "Classification":
         load_path = "D:/Image_Data/FishData"
-        train_loader, validation_loader, test_loader = Classification_data_loader(load_path, transform_info)
+        train_loader, validation_loader, test_loader = Classification_data_loader(load_path, batch_size, transform_info)
     else:
         traincsvfile_path = "D:/Image_Data/Pascal/100examples.csv"
         testcsvfile_path = "D:/Image_Data/Pascal/test.csv"
         IMG_DIR = "D:/Image_Data/Pascal/images"
         LABEL_DIR = "D:/Image_Data/Pascal/labels"
         transform = Compose(transform_info)
-        train_loader, validation_loader, test_loader = Object_Detection_data_loader(traincsvfile_path,
-                                                                                    transform=transform,
-                                                                                    img_dir=IMG_DIR,
-                                                                                    label_dir=LABEL_DIR)
+        train_validation_set = Object_Detection_data_loader(traincsvfile_path, transform=transform,
+                                                             img_dir=IMG_DIR, label_dir=LABEL_DIR)
+        test_set = Object_Detection_data_loader(testcsvfile_path, transform=transform,
+                                                             img_dir=IMG_DIR, label_dir=LABEL_DIR)
 
-    train_model(device=device, model=model, epochs=epoch, patience=patience, train_loader=train_loader,
+        train_set_num = int(0.8 * len(train_validation_set))
+        validation_set_num = len(train_validation_set) - train_set_num
+
+        train_set, validation_set = random_split(train_validation_set, [train_set_num, validation_set_num])
+
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+        validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+        test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+    train_model(device=device, model=model, model_type=model_type, epochs=epoch, validation_epoch=validation_epoch,
+                learning_rate=learning_rate, patience=patience, train_loader=train_loader,
                 validation_loader=validation_loader, test_loader=test_loader, save_path=save_path)
 
 
