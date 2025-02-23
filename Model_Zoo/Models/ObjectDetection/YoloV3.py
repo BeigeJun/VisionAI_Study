a402cc8db0e09db5c83e0b33d262afff84978d54
Seo_Jun_ <wns201401@naver.com>
2025-02-23 13:40:00 +0900

    YoloV3 모델 완성

--- Model_Zoo/Models/ObjectDetection/YoloV3.py
+++ Model_Zoo/Models/ObjectDetection/YoloV3.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from Model_Zoo.Models.Util.ModelBase import modelbase
+from torchvision import transforms
 
 
 class BasicCNNBlock(nn.Module):
@@ -29,6 +30,7 @@ class ResidualBlock(nn.Module):
             layers.append(BasicCNNBlock(In_Output//2, In_Output, kernel_size=3, padding=1))
         self.Residual_step = nn.Sequential(*layers)
         self.use_Residual = use_Residual
+        self.num_repeats = Repeat
 
     def forward(self, x):
         if self.use_Residual:
@@ -69,13 +71,77 @@ class DarkNet53(nn.Module):
 
         self.layers = []
         for i, (Input, Output, Kernel, Stride, Residual_repeat) in enumerate(self.cfg):
-            self.layers.append(BasicCNNBlock(Input=Input, Output=Output, Kernel=Kernel, Stride=Stride, padding=1))
+            self.layers.append(BasicCNNBlock(Input=Input, Output=Output, kernel_size=Kernel, stride=Stride, padding=1))
             self.layers.append(ResidualBlock(In_Output=Output, Repeat=Residual_repeat)) if Residual_repeat != 0 else None
 
     def forward(self, x):
         outputs = []
         for layer in self.layers:
             x = layer(x)
-            if isinstance(layer, ResidualBlock) and layer.Residual_step[1].conv.out_channels in[256, 512, 1024]:
+            if isinstance(layer, ResidualBlock) and layer.num_repeats in [8, 8, 4]:
                 outputs.append(x)
         return outputs
+
+
+class YoloV3(modelbase):
+    def __init__(self, num_classes=80):
+        super().__init__()
+        self.transform_info = transforms.Compose([
+            transforms.Resize((416, 416)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        self.Backbone = DarkNet53()
+        self.num_classes = num_classes
+        self.Input = 1024
+        self.cfg =[
+            #Output, Kernel, ScalePrediction or UpSample
+            [512, 1, "X"],
+            [1024, 3, "S"],
+            [256, 1, "U"],
+            [256, 3, "X"],
+            [512, 3, "S"],
+            [128, 1, "U"],
+            [128, 1, "X"],
+            [256, 3, "S"]]
+        self.layers = self.create_layers()
+        print("")
+
+    def create_layers(self):
+        layers = []
+        Input=self.Input
+        for Output, Kernel, layer_type in self.cfg:
+            layers.append(BasicCNNBlock(Input, Output, kernel_size=Kernel, stride=1, padding=Kernel // 2))
+            Input = Output
+            if layer_type == "S":
+                layers.append(ResidualBlock(Input, use_Residual=False, Repeat=1))
+                layers.append(BasicCNNBlock(Input, Input//2, kernel_size=Kernel, padding=Kernel // 2))
+                layers.append(ScalePrediction(Input//2, self.num_classes))
+                Input = Input // 2
+            elif layer_type == "U":
+                layers.append(nn.Upsample(scale_factor=2))
+                Input = Input * 3
+        return layers
+
+    def forward(self, x):
+        outputs = []
+
+        backbone_outputs = self.Backbone(x)
+        x = backbone_outputs[-1]
+        backbone_outputs.pop()
+        for i, layer in enumerate(self.layers):
+            if isinstance(layer, ScalePrediction):
+                outputs.append(layer(x))
+                continue
+
+            x = layer(x)
+
+            if isinstance(layer, nn.Upsample):
+                x = torch.cat([x, backbone_outputs[-1]], dim=1)
+                backbone_outputs.pop()
+
+        return outputs
+
+    def return_transform_info(self):
+        return self.transform_info
